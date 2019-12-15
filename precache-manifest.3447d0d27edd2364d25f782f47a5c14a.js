e092508d4a80fa7d906adc8080fd92c0713071a3
Peter Ayello-Wright <peter.ayellowright@gmail.com>
2019-12-15 21:23:36 +0000

    Updates

---
+++ precache-manifest.3447d0d27edd2364d25f782f47a5c14a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "49eb900ea11ae43d6090",
+    "url": "/static/css/main.18c8f6e5.chunk.css"
+  },
+  {
+    "revision": "49eb900ea11ae43d6090",
+    "url": "/static/js/main.b5f20f8c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48b213ac833999f71651",
+    "url": "/static/js/2.843e377e.chunk.js"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "27ec3b3d68a0a0e77fb4af85b866b587",
+    "url": "/index.html"
+  }
+];
